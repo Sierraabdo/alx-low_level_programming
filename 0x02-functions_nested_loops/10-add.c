1ca8d8cbc8fb08266fae60b9159f091957045896
Sierraabdo <said86cavalier@gmail.com>
2023-03-22 18:44:38 -0700

    add numbers file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
- * add - Adds two integers
- * @a: The first number to be computed
- * @b: The seconde number to be computed
- * Return: The result of add function
+ * add - Adds two values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
  */
 
-int add(int a, int b)
+int add(int n1, int n2)
 {
-	int result;
-
-	result = a + b;
-	_putchar(result);
-	_putchar('\n');
-	return (result);
+	return (n1 + n2);
 }
