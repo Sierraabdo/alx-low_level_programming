20493d168e4de510ce2d3b5972a4709d3e21f0cc
Sierraabdo <said86cavalier@gmail.com>
2023-03-22 18:40:29 -0700

    add numbers file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: The first number to be computed
+ * @b: The seconde number to be computed
+ * Return: The result of add function
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	_putchar(result);
+	_putchar('\n');
+	return (result);
+}
