3a577d91c7feab675ff31ec0b1ba29a33ac8d988
Sierraabdo <said86cavalier@gmail.com>
2023-03-30 18:00:23 -0700

    2 strncpy file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,6 +19,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
-		dest[index] = '\n';
+		dest[index] = '\0';
 	return (dest);
 }
