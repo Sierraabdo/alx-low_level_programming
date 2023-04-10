0b2f68dd36e20ad941a9dbf910f7ee2665041c24
Sierraabdo <said86cavalier@gmail.com>
2023-04-10 16:18:42 -0700

    multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,20 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int m;
-	int a = strtol(argv[1], NULL, 10);
-	int b = strtol(argv[2], NULL, 10);
-
-	m = a * b;
+	int n1, n2;
 
 	if (argc == 3)
 	{
-		printf("%d\n", m);
-		return (0);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
 	}
-	else if (argc != 3)
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
